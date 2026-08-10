941a04e97a9ed2561b483259c9de17e60ad145ff
LEONARDO BEVONESI DE CONTO <leonardo_conto@estudante.sesisenai.org.br>
2026-08-10 20:54:41 -0300

    feito a questão 6

--- script.js
+++ script.js
@@ -60,7 +60,9 @@ document.getElementById("btn5").addEventListener("click", function () {
 // Dica: leia o valor atual com .textContent, converta para
 // numero com Number() e some 1
 
-
+document.getElementById("btn6").addEventListener("click", function () {
+    document.getElementById("contador6").textContent = Number(document.getElementById("contador6").textContent) + 1
+})
 
 // ---------- Exercicio 7 ----------
 // Ao clicar no botao, pegue o valor digitado no #input7
