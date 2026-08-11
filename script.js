4e8b742d88badb95c2681c931366a4f8e4760e9b
LEONARDO BEVONESI DE CONTO <leonardo_conto@estudante.sesisenai.org.br>
2026-08-10 21:40:35 -0300

    feito a questão 10

--- script.js
+++ script.js
@@ -98,3 +98,6 @@ document.getElementById("btn9").addEventListener("click", function () {
 //
 // Dica: use .src = "nova-url"
 
+document.getElementById("btn10").addEventListener("click", function () {
+    document.getElementById("imagem10").src = "https://placecats.com/300/200"
+})
